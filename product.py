80e6158b225e9c12d34e60ba2d3cdc2645efd20b
lwyg05 <lwyg.03@gmail.com>
2026-05-16 12:41:33 +0300

    Create product.py

---
+++ product.py
@@ -0,0 +1,21 @@
+%%writefile product.py
+
+def create_product(product_id, name, category, price, quantity):
+
+    return {
+        "id": str(product_id),
+        "name": str(name),
+        "category": str(category),
+        "price": float(price),
+        "quantity": int(quantity)
+    }
+
+
+def display_product(product):
+
+    print(f"ID: {product['id']}")
+    print(f"Name: {product['name']}")
+    print(f"Category: {product['category']}")
+    print(f"Price: ${product['price']:.2f}")
+    print(f"Quantity: {product['quantity']}")
+    print("-" * 40)
